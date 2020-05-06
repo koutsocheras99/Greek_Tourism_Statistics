3f6d4f4cca5a180d2ce15e973b7fcea71a231e13
Ioannis Koutsocheras <37080724+koutsocheras99@users.noreply.github.com>
2020-05-07 01:50:44 +0300

    Update tourist_per_year.py

--- tourist_per_year.py
+++ tourist_per_year.py
@@ -1,9 +1,13 @@
 import xlrd
 import re
+import matplotlib.pyplot as plt
+
+# this could be done in a more simplier way (not using dictionary but lists(check tourist_per_quarter.py)) but it was implemented together with countries_most.py
 
 def most_tourists():
 
     years = ['2011','2012','2013','2014','2015']
+    plot_list = []
 
     for year in years:
         # no need to go to every file. just the Q4 at the last sheet contains the yearly required data
@@ -28,17 +32,18 @@ def most_tourists():
         # iterating in the countries, number of tourist columns
         for country, num_tourist in zip(countries, num_tourists):
             # removing non irrelevant records (ΕΤΟΣ)
-            cleared = re.sub('\d', '', str(num_tourist))
+            cleared = re.sub('[^0-9.]', '', str(num_tourist))
             
             if cleared:
                 # append to the dictionary with the certain format
                 country_tourist.update({country: num_tourist})
+                # print(cleared)
 
         #print(country_tourist)
 
         # sorting the initial dictionary based on the number of tourists (x[1])
         sorted_numTour_dict = sorted(country_tourist.items(), key=lambda  x: x[1], reverse=True)
-
+        
         # print(sorted_numTour_dict)
 
         for key in sorted_numTour_dict:
@@ -48,10 +53,20 @@ def most_tourists():
             # for excluding empty key values and keys such as Λοιπά Κράτη Ευρώπης, Λοιπά κράτη Ασίας, etc (issue at Λίβανος - Συρία! that has 2 spaces) 
             if len(key[0])>0 and count_space<2:
                 # append to the top list 
-                country_tourist_top.append(key)
+                country_tourist_top.append(key[1])
 
         print(country_tourist_top[:1])
+        plot_list.append(country_tourist_top[:1])
 
+    # remove the circular reference      
+    plot_list = sum(plot_list, [])
 
+    plt.bar(years, plot_list, color='#0D69A6', width=0.35)
+    plt.title('Total Tourists per Year')
+    plt.xlabel('Years')
+    plt.ylabel('Number of Tourists')
+    plt.show()
+    
+    
 if __name__ == '__main__':
     most_tourists()
