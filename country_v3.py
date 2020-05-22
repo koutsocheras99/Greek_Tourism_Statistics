186ffcf077b73e6ce512c4d3aac9c1bc7a01d861
Ioannis Koutsocheras <37080724+koutsocheras99@users.noreply.github.com>
2020-05-22 20:13:21 +0300

    Update country_v3.py

--- country_v3.py
+++ country_v3.py
@@ -1,12 +1,47 @@
 import xlrd
 import re
+from collections import defaultdict
+import matplotlib.pyplot as plt
 
-def country_most_tourists(number_top):
+
+def init_country_dict():
+    # no need to go to every file. just the Q4 at the last sheet contains the yearly required data
+    dataset_path = 'dataset/Αφίξεις μη κατοίκων από το εξωτερικό ανά χώρα προέλευσης/'+'2015'+'-Q4.xls'
+
+    # opening the file
+    data = xlrd.open_workbook(dataset_path)
+
+    # determine the last sheet (month = December)
+    december_sheet = data.sheet_by_index(11)
+
+    # defining the countries column
+    countries = december_sheet.col_values(1)
+    
+    country_tourist = {}
+
+    # iterating in the countries column
+    for country in countries:
+
+        count_space = len(re.findall(' ', country))
+        trash = re.findall(':', country)
+        
+        if len(country)>0 and count_space<2 and not trash:
+            # 'append-initialize' each country in the returning dictionary
+            country_tourist.update({country: int(0)})
+
+    return country_tourist
+
+
+def country_most_tourists(numberOfCountries, start_year, end_year):
+
+    country_tourist = init_country_dict()
+
+    # print(country_tourist)
 
     years = ['2011','2012','2013','2014','2015']
 
-    for year in years:
-        
+    for year in years[(start_year-2011):(end_year-2011+1)]:
+       
         # no need to go to every file. just the Q4 at the last sheet contains the yearly required data
         dataset_path = 'dataset/Αφίξεις μη κατοίκων από το εξωτερικό ανά χώρα προέλευσης/'+str(year)+'-Q4.xls'
 
@@ -15,43 +50,50 @@ def country_most_tourists(number_top):
 
         # determine the last sheet (month = December)
         december_sheet = data.sheet_by_index(11)
-
-        # defining the countries column
-        countries = december_sheet.col_values(1)
-
-        # defining the number of tourists column
-        num_tourists = december_sheet.col_values(3)
-
-        country_tourist = {}
-
-        country_tourist_top = []
-
-        # iterating in the countries, number of tourist columns
-        for country, num_tourist in zip(countries, num_tourists):
+        
+        # iterating in the countries, percentage of tourists columns
+        for country, percentage_tourists in zip(december_sheet.col_values(colx=1,start_rowx=71,end_rowx=None), december_sheet.col_values(colx=6,start_rowx=71,end_rowx=None)):
             # removing non irrelevant records (letters) (example -> ΕΤΟΣ) 
-            cleared = re.sub('[^0-9.]', '', str(num_tourist))
+            cleared = re.sub('[^0-9.]', '', str(percentage_tourists))
+            # check countries.py
+            count_space = len(re.findall(' ', country))
+            trash = re.findall(':', country)
             
-            if cleared:
-                # append to the dictionary with the certain format
-                country_tourist.update({country: num_tourist})
+            if cleared and len(country)>0 and count_space<2 and not trash:
+                # increment to each country the median percentage of tourists per year
+                country_tourist[country] = country_tourist.get(country, 0) + percentage_tourists/len(years)
+                
 
-        # print(country_tourist)
+    sorted_numTour_dict = sorted(country_tourist.items(), key=lambda  x: x[1], reverse=True)
 
-        # sorting the initial dictionary based on the number of tourists (x[1])
-        sorted_numTour_dict = sorted(country_tourist.items(), key=lambda  x: x[1], reverse=True)
+    # print(country_tourist.values())
 
-        # print(sorted_numTour_dict)
+    # matplotlib stuff
+    labels = []
+    sizes = []
+    other_countries_size = 0
 
-        for key in sorted_numTour_dict:
-            # count space on the sorted dictionary keys for future operations
-            count_space = len(re.findall(' ', key[0]))
+    for k in sorted_numTour_dict[1:numberOfCountries+1]:
+        labels.append(k[0])
+        sizes.append(k[1])
+    
+    # trying putting the following 5 lines in comments to see the difference in the pie chart
+    # me liga logia -> an exw xwra_a, xwra_b me percentages 15% h kathemia sto geniko sunolo, sto chart tha mou bgoun 50% h kathemia pragma poy einai lathos
+    for k in sorted_numTour_dict[numberOfCountries+1:]:
+        other_countries_size +=  k[1]
 
-            # for excluding empty key values and keys such as Λοιπά Κράτη Ευρώπης, Λοιπά κράτη Ασίας, etc (issue at Λίβανος - Συρία! that has 2 spaces) 
-            if len(key[0])>0 and count_space<2:
-                # append to the top list 
-                country_tourist_top.append(key)
+    sizes.append(other_countries_size)
+    labels.append('Υπόλοιπες Χώρες')
+    
+    print(sorted_numTour_dict[1:numberOfCountries+1])
+    
+    plt.pie(sizes, labels=labels, autopct='%1.1f%%', shadow=True, startangle=140)
 
-        print(country_tourist_top[1:number_top+1])
+    plt.axis('equal')
+    plt.show()
+
+    # print to see the small fault(~2% ) countries such as Λοιπές Χώρες were excluded.
+    # print(other_countries_size)
 
 if __name__ == '__main__':
-    country_most_tourists(5)
+    country_most_tourists(8, start_year= 2011, end_year= 2015)
